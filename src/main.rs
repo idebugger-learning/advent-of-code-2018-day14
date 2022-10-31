f6451a004de45f1ab47624f9c9e64403d2b84584
iDeBugger Freeman <me@idebugger.io>
2022-10-31 21:25:18 +0300

    Solution part one

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+const INPUT: usize = 793031;
+
 fn main() {
-    println!("Hello, world!");
+    let mut recipies = vec![3, 7];
+    let mut elf1 = 0;
+    let mut elf2 = 1;
+
+    while recipies.len() < INPUT + 10 {
+        let mut sum = recipies[elf1] + recipies[elf2];
+        let mut sum_digits = vec![];
+        loop {
+            sum_digits.push(sum % 10);
+            sum /= 10;
+
+            if sum == 0 {
+                break;
+            }
+        }
+        for &digit in sum_digits.iter().rev() {
+            recipies.push(digit);
+        }
+
+        elf1 = (elf1 + recipies[elf1] + 1) % recipies.len();
+        elf2 = (elf2 + recipies[elf2] + 1) % recipies.len();
+    }
+
+    let result = recipies[INPUT..INPUT + 10]
+        .iter()
+        .map(|&score| score.to_string().chars().collect::<Vec<_>>())
+        .flatten()
+        .collect::<String>();
+    println!("Scoreboard len {}", recipies.len());
+    println!("Result: {}", result);
 }
